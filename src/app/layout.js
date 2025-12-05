35e5e82b080033f7ee2f56fd97c27124370dd60c
CollectInte <info@collectintel.in>
2025-12-05 17:04:51 +0530

    Add Google Analytics and Tag Manager scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
-import '@/styles/styles.scss';
+import "@/styles/styles.scss";
 import GlobalProvider from "./GlobalProvider";
 
 const plusJakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
@@ -7,13 +7,60 @@ const plusJakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 export const metadata = {
   title: "Ridgeveda",
   description: "",
-  icons:[{url: '/favicon.png'}]
+  icons: [{ url: "/favicon.png" }],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+
+        {/* Google Analytics Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17774094068"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17774094068');
+            `,
+          }}
+        />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T6X8FJB5');
+            `,
+          }}
+        />
+
+      </head>
       <body className={plusJakarta.className}>
+        {/* NoScript GTM */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T6X8FJB5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <GlobalProvider>
           {children}
           <div id="nav-full" />
